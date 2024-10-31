aeaed5b587dc39fe57707360c6b4f4200416ea44
ChristianMcNachtan <christian.mcnachtan@gmail.com>
2024-10-30 21:37:30 -0400

    added highlight for active nav link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,21 @@
+'use client'
+
+import { usePathname} from 'next/navigation'
 import React from 'react'
+import Link from 'next/link'
 
 const Navbar = () => {
+    
+    const pathname = usePathname();
+    
   return (
     <div>
         <div id='navbar'>
-            <a href="/">Home</a>
-            <a href="/blog">Blog</a>
-            <a href="/about">About</a>
-            <a href="/contact">Contact</a>
-            <a href="/dashboard">Dashboard</a>
+            <Link href="/" className={ `${pathname === '/' ? 'bg-white text-black' : ''}` }>Home</Link>
+            <Link href="/blog" className={ `${pathname === '/blog' ? 'bg-white text-black' : ''}`}>Blog</Link>
+            <Link href="/about" className={ `${pathname === '/about' ? 'bg-white text-black' : ''}` }>About</Link>
+            <Link href="/contact" className={ `${pathname === '/contact' ? 'bg-white text-black' : ''}` }>Contact</Link>
+            <Link href="/dashboard" className={ `${pathname === '/dashboard' ? 'bg-white text-black' : ''}` }>Dashboard</Link>
         </div>
       
     </div>
